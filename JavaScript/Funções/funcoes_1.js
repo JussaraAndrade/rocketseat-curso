ce3f3f497b0fa15187c067a86e8159b72a2e0af0
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-10 20:08:40 -0300

    feat: funções

---
+++ JavaScript/Funções/funcoes_1.js
@@ -0,0 +1,19 @@
+// criar um aplicativo de frases motivacionais
+
+//Declaração da função
+//function statement
+//Funções; agrupamento de códigos e reutilização de código.
+function createPhrases() {
+  console.log("Estudar é muito bom");
+  console.log("Paciência e persistência");
+  console.log("Revisão é mãe do aprendizado");
+}
+
+//executar a função
+//rodar, chamar, invocar
+//execute, run, call, invoke
+createPhrases();
+createPhrases();
+
+console.log("fim do programa");
+
