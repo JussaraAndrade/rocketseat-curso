2cd41035a6fd04841eb4a04d57a2ad704d5cb3a1
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 14:34:39 -0300

    feat: operadores de atribuição

--- JavaScript/Expressões e Operadores/scripts_1.js
+++ JavaScript/Expressões e Operadores/scripts_1.js
@@ -86,5 +86,43 @@ console.log(one <= 1)//true
 console.log(one <= 0)//false
 
 
+// Operadores de atribuição (Assignment)
+let x
+
+console.log(x)
+
+// assignment
+x = 1
+console.log(x)
+
+// addition assignment
+// x = x + 2
+x += 2
+console.log(x)
+
+// subtraction assignment
+// x = x - 1
+x -= 1
+console.log(x)
+
+// multiplication assignment
+// x = x * 2
+x *= 2
+console.log(x)
+
+// division assignment
+// x = x / 2
+x /= 2
+console.log(x)
+
+// remainder, exponetiation
+//x1 %= 2
+x **=2
+console.log(x)
+
+
+
+
+
 
 
