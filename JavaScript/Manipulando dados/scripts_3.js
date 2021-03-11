40dc813ce69cc46d36cd46aab6ea3dd6238b042b
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-10 22:37:52 -0300

    feat: manipulando arrays

--- JavaScript/Manipulando dados/scripts_3.js
+++ JavaScript/Manipulando dados/scripts_3.js
@@ -40,3 +40,30 @@ console.log(["a", "b", "c"].length)
 
 let word = "manipulação"
 console.log(Array.from(word))
+
+
+
+let techs = ["html", "css", "js"]
+
+// adicionar um item no fim
+techs.push("nodejs")
+
+// adicionar no começo
+techs.unshift("sql")
+
+// remover no fim
+//techs.pop()
+
+// remover no começo
+//techs.shift()
+
+// pegar somente alguns elementos do array
+//console.log(techs.slice(1,3))
+
+// remover 1 ou mais items em qualquer posição do array
+//techs.splice(1, 3)
+
+// encontrar a posição de um elemento no array
+let index = techs.indexOf('nodejs')
+techs.splice(index, 1)
+console.log(techs)
