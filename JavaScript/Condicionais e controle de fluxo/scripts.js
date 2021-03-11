6a5afbf6b13e4d1406a02d2df0157e0a472dfe25
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 16:05:08 -0300

    feat: controle de fluxo da aplicação

---
+++ JavaScript/Condicionais e controle de fluxo/scripts.js
@@ -0,0 +1,11 @@
+// Control flow
+
+//Executada linha a linha
+console.log('a')
+console.log('b')
+
+//se estiver sujo
+console.log('lavar o copo')
+
+//se não estiver sujo
+console.log('servir café')
