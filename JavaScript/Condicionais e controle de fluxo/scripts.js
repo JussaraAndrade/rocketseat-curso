801745c21d035879d1914ef4ca8d9f9c42578cf8
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 16:18:30 -0300

    feat: if e else

--- JavaScript/Condicionais e controle de fluxo/scripts.js
+++ JavaScript/Condicionais e controle de fluxo/scripts.js
@@ -9,3 +9,39 @@ console.log('lavar o copo')
 
 //se não estiver sujo
 console.log('servir café')
+
+
+// if...else - muda o fluxo 
+
+//Modelo 1:
+let temperature = 38
+
+if(temperature >= 37){
+    console.log('Febre')
+}else{
+    console.log('Saudável')
+}
+
+if(temperature >= 37.5){
+    console.log('Febre alta')
+}else if(temperature < 37.5 && temperature >= 37){
+    console.log('Febre moderada')
+}else{
+    console.log('Saudável')
+}
+
+//Modelo 2:
+
+let temperature_1 = 36.9
+let highTemperature_1 = temperature_1 >= 37.5
+let mediumTemperature_1 = temperature_1 >= 37.5 && temperature_1 >= 37
+
+if(highTemperature_1){
+    console.log('Febre alta')
+}else if(mediumTemperature_1){
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
+
+
