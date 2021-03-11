4b8267c96b735bb481fba4d519369eb790791a8b
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 16:34:25 -0300

    feat: switch

--- JavaScript/Condicionais e controle de fluxo/scripts.js
+++ JavaScript/Condicionais e controle de fluxo/scripts.js
@@ -45,3 +45,33 @@ if(highTemperature_1){
 }
 
 
+// switch
+
+function calculate(number1, operator, number2) {
+    let result = 0
+  
+    switch (operator) {
+      case "+":
+        result = number1 + number2;
+        break;
+      case "-":
+        result = number1 - number2;
+        break;
+      case "*":
+        result = number1 * number2;
+        break;
+      case "/":
+        result = number1 / number2;
+        break;
+      default:
+        console.log("não implementado");
+        break;
+    }
+    return result
+  }
+
+console.log(calculate(4, '+', 8))
+console.log(calculate(4, '-', 8))
+console.log(calculate(4, '*', 8))
+console.log(calculate(4, '/', 8))
+console.log(calculate(4, '%', 8))
