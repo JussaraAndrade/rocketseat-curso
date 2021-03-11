ff20ea3d81b4c29785f4620f7e156c8fd1ae1ded
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 13:14:33 -0300

    feat: new

--- JavaScript/Expressões e Operadores/scripts.js
+++ JavaScript/Expressões e Operadores/scripts.js
@@ -21,3 +21,17 @@ console.log(true ? 'alo' : 'nada')
 console.log(false ? 'alo' : 'nada')
 
 
+/*
+    new
+
+    * left-hand-side expression
+    * criar um novo objeto
+*/
+
+let name = new String('Mayk')
+name.surName = "Brito"
+let age = new Number(23)
+console.log(name.surName, age)
+
+let date = new Date('2020-12-01')
+console.log(date.__proto__)
