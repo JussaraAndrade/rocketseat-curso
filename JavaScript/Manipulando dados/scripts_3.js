3f9b3f419586364e02d03ea44ebb8f1eb97ff2e0
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-10 22:03:27 -0300

    feat: separando strings

--- JavaScript/Manipulando dados/scripts_3.js
+++ JavaScript/Manipulando dados/scripts_3.js
@@ -4,3 +4,19 @@
 
 let phrase = "Eu quero viver o Amor!"
 console.log(phrase.includes("Amor"))
+
+
+
+//Manipulando String e Arrays
+
+/* 
+Separe um texto que contem espaços, em um novo array onde cada texto é uma posição do array.
+Depois disso, transforme o array em um texto onde eram espaços, coloque _
+*/
+
+let phrase_1 = "Eu quero viver o Amor!"
+let myArray = phrase_1.split(" ")
+//join; juntar
+let phraseWithUnderscore = myArray.join("_")
+console.log(myArray)
+console.log(phraseWithUnderscore.toLowerCase())
