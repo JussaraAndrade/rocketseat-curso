3a7273b2490176ddb16ddf210e21935089775f34
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-10 21:50:34 -0300

    feat: maiúsculas e minúsculas

--- JavaScript/Manipulando dados/scripts_2.js
+++ JavaScript/Manipulando dados/scripts_2.js
@@ -18,3 +18,10 @@ console.log(String(number_2).length)
 let number_3 = 654.6546543
 //Troca o ponto por vírgula
 console.log(number_3.toFixed(2).replace(".", ","))
+
+//Transforme letras minúsculas em maiúsculas. Faça o contrário disso também
+
+let word_2 = "Programar é muito bacana!"
+let word_3 = "PROGRAMAR É MUITO BACANA!"
+console.log(word_2.toUpperCase())
+console.log(word_3.toLowerCase())
