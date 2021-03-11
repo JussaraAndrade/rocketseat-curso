216ecc0ebfda7a415c9090e896501e644abbaced
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-10 22:25:25 -0300

    feat: strings para arrays

--- JavaScript/Manipulando dados/scripts_3.js
+++ JavaScript/Manipulando dados/scripts_3.js
@@ -34,3 +34,9 @@ console.log(myArray_2)
 
 //Contar elementos de um array
 console.log(["a", "b", "c"].length)
+
+
+//Transformar uma cadeia de caracteres em elementos de um array
+
+let word = "manipulação"
+console.log(Array.from(word))
