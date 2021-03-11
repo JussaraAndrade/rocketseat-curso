9730043f9f9ba856976bc2c533ec60cc22b2c577
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 14:10:17 -0300

    feat: operadores de comparação

--- JavaScript/Expressões e Operadores/scripts_1.js
+++ JavaScript/Expressões e Operadores/scripts_1.js
@@ -43,6 +43,48 @@ let total_2 = (2 + 3) * 5
 console.log(total_2) 
 
 
+//Operadores de comparação
+
+//Irá comparar valores e retornar um Boolean como resposta à comparação
+
+let one = 1
+let two = 2
+
+// = recebe
+let name =  'João'
+
+// == igual
+console.log(one == 1) // true
+console.log(one == "1") //true
+
+// != diferente
+console.log(one != two) //true
+console.log(one != 1) //false
+console.log(one != "1") //false
+
+// === estritamente igual (verifica se o tipo é igual, não o valor)
+console.log(one === "1") //false
+console.log(one === 1)//true
+
+// !== estritamente diferente
+console.log(two !== "2")// true
+console.log(two !== 2)// false
+
+// > Maior
+console.log(one > two)//false
+
+// >= Maior ou igual
+console.log(one >= 1)//true
+console.log(two >= 1)//true
+
+// < Menor
+console.log(one < two)// true
+
+// <= Menor ou igual
+console.log(one <= two)//true
+console.log(one <= 1)//true
+console.log(one <= 0)//false
+
 
 
 
