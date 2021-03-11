bff4af790b6d30866ca1854ea38ee3969336ba7a
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-10 21:32:32 -0300

    feat: Prototype e Type Conversion coersion

---
+++ JavaScript/Manipulando dados/scripts.js
@@ -0,0 +1,20 @@
+/*
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    * __proto__ ; {}: cadeia de protótipos
+
+*/
+
+/*
+    Type conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+
+*/
+
+console.log(Number('9') + 5)
+
+
+
