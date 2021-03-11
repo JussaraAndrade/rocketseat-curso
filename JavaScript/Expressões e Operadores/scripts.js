be6806b48f4b3e5c028e1ebd2d621a4e90108d91
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-11 13:09:34 -0300

    feat: expressões e operadores

---
+++ JavaScript/Expressões e Operadores/scripts.js
@@ -0,0 +1,23 @@
+/* Expressões e Operadores
+
+- Expressions
+- Operators
+    Binary
+    Unary 
+    Ternary
+*/
+
+let number = 1;
+
+(function(){
+    console.log('alo')
+})
+
+console.log(number + 1)
+console.log(++number)
+console.log(--number)
+console.log(typeof number)
+console.log(true ? 'alo' : 'nada')
+console.log(false ? 'alo' : 'nada')
+
+
