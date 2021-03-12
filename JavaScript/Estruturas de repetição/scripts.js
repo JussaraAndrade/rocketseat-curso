523ac1f39ab002bb11144d96f9b32268f5326293
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-12 13:29:58 -0300

    feat: for

---
+++ JavaScript/Estruturas de repetição/scripts.js
@@ -0,0 +1,28 @@
+//Estrutura de repetição
+// for
+// break - para a execução do loop
+// continue - pula a execução do momento
+
+
+for(let i = 1; i < 10; i++){
+    console.log('De 1 a 9: ' +i)
+}
+
+for(let i = 0; i < 10; i++){
+    console.log('De 0 a 9: ' +i)
+}
+
+for(let i = 100; i > 0; i--){
+    if(i === 50){
+        break;
+    }
+    console.log(i)
+}
+
+for(let i = 10; i > 0; i--){
+    if(i === 5){
+        continue;
+    }
+    console.log(i)
+}
+
