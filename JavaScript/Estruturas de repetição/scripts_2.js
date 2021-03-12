db4751c81d92b682ab4ec5b5d478b96905f48901
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-12 14:44:44 -0300

    feat: for in

--- JavaScript/Estruturas de repetição/scripts_2.js
+++ JavaScript/Estruturas de repetição/scripts_2.js
@@ -12,3 +12,18 @@ for(let name of names){
     console.log(name)
 }
 
+
+// for...in
+
+let person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6
+}
+
+for(property in person){
+    console.log(property)
+    console.log('Name: ' +person.name)
+    console.log('Age: ' +person["age"])
+    console.log(`Weight: ${person.weight}`)
+}
