c7ce858ad8a87eda0325c4baf9b8784ae1a63a47
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-20 12:47:12 -0300

    feat: querySelectorAll

--- Preparação astronautas/DOM/Selecionando elementos/script.js
+++ Preparação astronautas/DOM/Selecionando elementos/script.js
@@ -1,6 +1,19 @@
-//querySelector() - Pega o primeiro elemento que aparecer
+//querySelectorAll() - irá trazer uma coleção de nó
 
-const element_1 = document.querySelector('.one')
-const element_2 = document.querySelector('[src]')
+//Argumento pela classe
+const element_1 = document.querySelectorAll('.one')
+
+//Argumento pelo id
+const element_2 = document.querySelectorAll('#two')
+
 console.log(element_1)
 console.log(element_2)
+
+//Plural - elements
+const elements_1 = document.querySelectorAll('#two')
+
+elements_1.forEach(el => console.log(el))
+
+const elements_2 = document.querySelectorAll('.one')
+
+elements_2.forEach(el => console.log(el))
