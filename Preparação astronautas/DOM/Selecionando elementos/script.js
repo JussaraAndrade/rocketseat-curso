5b632075fae506c4fc0de0f2107792eb9bc07e60
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-20 12:37:13 -0300

    feat: querySelector

--- Preparação astronautas/DOM/Selecionando elementos/script.js
+++ Preparação astronautas/DOM/Selecionando elementos/script.js
@@ -1,4 +1,6 @@
-//getElementByTagName()
+//querySelector() - Pega o primeiro elemento que aparecer
 
-const element = document.getElementsByTagName('h1');
-console.log(element)
+const element_1 = document.querySelector('.one')
+const element_2 = document.querySelector('[src]')
+console.log(element_1)
+console.log(element_2)
