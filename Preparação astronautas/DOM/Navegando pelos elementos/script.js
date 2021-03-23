dea9879ec995da7a69f4f1e15fb30545e1409bc9
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-23 13:15:43 -0300

    feat: parentElement e parentNode

---
+++ Preparação astronautas/DOM/Navegando pelos elementos/script.js
@@ -0,0 +1,11 @@
+// Navegando pelos elementos
+// parentNode parentElement
+
+//Elemento pai
+const body = document.querySelector('body')
+
+console.log(body.parentNode)
+
+const element = document.querySelector('h1')
+
+console.log(element.parentNode)
