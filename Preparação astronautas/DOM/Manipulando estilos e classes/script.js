efa077a15d4cd32036a5adc82aa405bef889e2e3
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-03-23 13:03:54 -0300

    feat: classList

--- Preparação astronautas/DOM/Manipulando estilos e classes/script.js
+++ Preparação astronautas/DOM/Manipulando estilos e classes/script.js
@@ -1,6 +1,9 @@
 // Alterando estilos
+// classList
 
 const element = document.querySelector('body')
 
-element.style.backgroundColor = "#f9f3D2"
-console.log(element.style.backgroundColor)
+element.classList.add('active', 'green')
+console.log(element.classList)
+//element.classList.remove('active')
+element.classList.toggle('active')
