d7a5af6e6be1bea20c1a4a95b5bb51f064e250b5
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-04-16 19:41:25 -0300

    feat: projeto inicial

---
+++ Frameworks React/Upload de arquivos/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { Container, Content } from './styles';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <Container>
+      <Content>
+      <GlobalStyle />
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
