76544da715c70147e72f87b5c677f33c194b9f0e
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-04-16 15:33:50 -0300

    feat: criação de compoentes Form

--- Frameworks React/Formulários  Unform/unformweb/src/App.js
+++ Frameworks React/Formulários  Unform/unformweb/src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
 
+import { Scope } from '@unform/core';
+import { Form } from '@unform/web';
+
+import Input from './components/Form/Input';
+
+const initialData = {
+  email: "diego@rocketseat.com.br",
+  address: {
+    city: 'Rio do Sul'
+  }
+};
+
+// const user = {
+//   name: 'Diego',
+//   address: {
+//     street: 'Rua teste',
+//     number: '123',
+//   }
+// }
+
 function App() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <div className="App">
-     <h1>oi</h1>
+      <h1>oi</h1>
+
+      <Form initialData={initialData} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input type="email" name="email" />
+
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="neighborhood" />
+          <Input name="city" />
+          <Input name="state" />
+          <Input name="number" />
+        </Scope>
+
+        <button type="submit">Enviar</button>
+      </Form>
     </div>
   );
 }
---
+++ Frameworks React/Formulários  Unform/unformweb/src/components/Form/Input.js
@@ -0,0 +1,23 @@
+import { useField } from '@unform/core';
+import React, { useEffect, useRef } from 'react';
+
+export default function Input({ name, ...rest }){
+    const inputRef = useRef(null);
+    const { fieldName, registerField, defaultValue, error } = useField(name);
+
+    console.log(inputRef.current);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value'
+        })
+    }, [fieldName, registerField]);
+    
+    return(
+        <input ref={inputRef} defaultValue={defaultValue} {...rest} />
+    );
+}
+
+// uncontrolled-form - não anotar os dados de um input enquanto o usuário digita
